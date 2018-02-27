f404cd335b5c369e577f2261f18e41a683fe03b0
kanchbubblu <36299426+kanchbubblu@users.noreply.github.com>
2018-02-27 17:32:47 +0530

    swapping

---
+++ swapping.py
@@ -0,0 +1,6 @@
+a=int(input("enter the first number:"))
+b=int(input("enter the second number:"))
+print("\n before swapping a=%d and b=%d" %(a, b))
+a, b=b, a
+print("\n after swapping a=%d and b=%d" %(a, b))
+print()
