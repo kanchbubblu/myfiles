dbf538f969d80a76a1105d3c3cf394a1b7c34148
kanchbubblu <36299426+kanchbubblu@users.noreply.github.com>
2018-02-27 17:59:58 +0530

    count no of character

---
+++ character.py
@@ -0,0 +1,11 @@
+string=input("enter the string:")
+char=0
+word=1
+for i in string:
+char=char+1
+  if(i==' '):
+    word=word+1 
+print("number of words:",word)
+print(word)
+print("number of char:",char)
+print(char)
