22ea4ec5f38f99f2f7e41c49edb78e3916d50d41
kanchbubblu <36299426+kanchbubblu@users.noreply.github.com>
2018-03-13 17:14:09 +0530

    find odd even position of given number

---
+++ oddevenposition.py
@@ -0,0 +1,16 @@
+def oddeven():
+	n=int(input())
+	l=[]
+	for i in range(n):
+		l.append(int(input()))
+	o,e=[],[]
+	for i in range(n):
+		if i%2==0 and l[i]%2!=0:
+			o.append(l[i])
+		elif i%2!=0 and l[i]%2==0:
+			o.append(l[i])
+	print(o)
+try:
+	oddeven()
+except:
+	print('invalid')
