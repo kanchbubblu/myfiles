429b718be8e260a4f2e1bc367496cecdf7d76dee
kanchbubblu <36299426+kanchbubblu@users.noreply.github.com>
2018-03-22 22:21:14 +0530

    Find area and volume of cuboid

---
+++ areaandvolume.py
@@ -0,0 +1,7 @@
+length=float(input("enter the length:"))
+width=float(input("enter the width:"))
+height=float(input("enter the height:"
+SA=2*(length*width+length*height+width*height)
+volume=length*width*height
+print("\n the surface of a cuboid =%2f"%SA)
+print("the volume of cuboid=%2f"%volume)
