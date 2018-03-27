2ad6416a9b3d7169247a6d0442915f77e36dc847
kanchbubblu <36299426+kanchbubblu@users.noreply.github.com>
2018-03-27 22:41:10 +0530

    Convert binary into hexadecimal

---
+++ binarytohex.py
@@ -0,0 +1,8 @@
+print=("enter 'x' for exit.")
+binary = input("enter any binary num:")
+   if binary =='x':
+       exit();
+    else:
+       temp=int(binary,2);
+       print(binary,"in ocatl=",oct(temp));
+       
