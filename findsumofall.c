eec7b9d082038b010a059844b1fcbd23d0b48ef7
kanchbubblu <36299426+kanchbubblu@users.noreply.github.com>
2018-05-21 16:17:36 +0530

    find sum of all elements

---
+++ findsumofall.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) 
+{
+	int k,array[50],i,sum=0;
+	scanf("%d",&n);
+	for(i=0;i<k;i++)
+	{
+		scanf("%d",&array[i]);
+	}
+	for(i=0;i<k;i++)
+	{
+		sum=sum+array[i];	
+	}
+	printf("\k%d",sum);
+	return 0;
+}
