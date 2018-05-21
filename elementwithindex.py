73fe8b6e4af8f32ef24199a532ece9e12dec675c
kanchbubblu <36299426+kanchbubblu@users.noreply.github.com>
2018-05-21 12:17:37 +0530

    print element with index

---
+++ elementwithindex.py
@@ -0,0 +1,8 @@
+def checkLen():
+days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+position = 0
+for i in days:
+    if i == "Monday":
+        position = i
+        print("Found it")
+        print(position)
