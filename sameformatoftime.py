ed1a286c6c9bb74ec84fb9bf4d0a07111e962876
kanchbubblu <36299426+kanchbubblu@users.noreply.github.com>
2018-05-21 14:41:40 +0530

    same format of given two time

---
+++ sameformatoftime.py
@@ -0,0 +1,10 @@
+m=15
+print('sum from 1-15 :'+str(m*(m+1)//2))
+s=15
+e=45
+sum=0
+for i in range(s,e+1):
+	if i%2==0:
+		continue
+	sum+=i
+print('sum of odd number from 15-45(included) :'+str(sum))
