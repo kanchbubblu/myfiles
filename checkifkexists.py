1c180e6e4b5eb913f229b68e42642415dfc58696
kanchbubblu <36299426+kanchbubblu@users.noreply.github.com>
2018-05-21 12:29:26 +0530

    check if k exists in given number

---
+++ checkifkexists.py
@@ -0,0 +1,14 @@
+def checkkn(l,k):
+	if k in l:
+		return 'yes'
+	return 'no'
+def main():
+	try:
+		n=int(input())
+		k=int(input())
+		l=[]
+		for i in range(n):
+			l.append(int(input()))
+		print(checkkn(l,k))
+	except:
+		print('invalid')
