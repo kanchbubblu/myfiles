9464f0fa02505d2996401e86f6532aa59b0e6e95
kanchbubblu <36299426+kanchbubblu@users.noreply.github.com>
2018-05-24 11:42:45 +0530

    lower and higher

---
+++ lowhigh.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    int a=20;
+    int b=40;
+    int c=30;
+    if(a<b)
+    {
+        printf("%d",a);
+    }
+    if(b<c)
+    {
+        printf("%d",c);
+    }
+}
