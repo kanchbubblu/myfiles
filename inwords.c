bcd57f5fb84590aec3dd532dd0489f22166edc63
kanchbubblu <36299426+kanchbubblu@users.noreply.github.com>
2018-05-24 11:51:44 +0530

    print in words

---
+++ inwords.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+int m;
+printf("enter your choice");
+scanf("%d",&m);
+switch(a)
+case1:printf("one");
+      break;
+case2:printf("two");
+      break;
+case3:printf("three");
+      break;
+case4:printf("four');
+      break;
+case5:printf("five");
+      break;
+default:printf("error");
+       break;
+getch();
+}
